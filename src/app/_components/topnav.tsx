e2b9cc417c2310a879040ebe5139b1ac13f6712a
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2024-04-23 15:58:36 +0800

    style: change text

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -4,7 +4,9 @@ import { SimpleUploadButton } from "./simple-upload-button";
 export function TopNav() {
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
-      <div>Gallery</div>
+      <div className="  bg-gradient-to-r from-emerald-400 from-20% to-yellow-300 bg-clip-text text-3xl font-bold leading-none text-transparent transition">
+        T3G<span className="">allery</span>
+      </div>
       <div className="flex flex-row items-center gap-4 ">
         <SignedOut>
           <SignInButton />
