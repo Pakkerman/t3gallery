612eabf958cbe7234c4c480a14b462f3a327cde8
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2024-04-25 22:11:48 +0800

    feat: start selection button

--- src/app/_components/gallery.tsx
+++ src/app/_components/gallery.tsx
@@ -2,28 +2,48 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Button } from "~/components/ui/button";
 import type { SelectImage } from "~/server/db/schema";
 
 export function Gallery({ images }: { images: SelectImage[] }) {
+  const [selecting, setSelecting] = useState(false);
+
   return (
     <section className="flex select-none flex-wrap justify-center gap-4 p-4">
       {images.map((item) => (
-        <Link key={item.key} href={`/img/${item.id}`}>
-          <GalleryItem item={item} />
+        <Link key={item.key} href={`${selecting ? "#" : "/img/" + item.id}`}>
+          <GalleryItem item={item} selecting={selecting} />
         </Link>
       ))}
+      <Button
+        className="fixed bottom-[2.5%] right-[5%] rounded-full"
+        onClick={() => setSelecting((prev) => !prev)}
+      >
+        {selecting ? "+" : "X"}
+      </Button>
     </section>
   );
 }
 
-function GalleryItem({ item }: { item: SelectImage }) {
+function GalleryItem({
+  item,
+  selecting,
+}: {
+  item: SelectImage;
+  selecting: boolean;
+}) {
   const [selected, setSelected] = useState(false);
+
+  useEffect(() => {
+    setSelected(false);
+  }, [selecting]);
 
   return (
     <li
       className={`flex h-36 w-48 flex-col border-2 ${selected && "border-red-400 p-2"} transition`}
       onClick={() => {
+        if (!selecting) return;
         setSelected((prev) => !prev);
       }}
     >
